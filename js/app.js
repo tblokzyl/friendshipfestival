5148a667a998158bdab3b7d7fbc35c824eee0257
ellisdesignca <34076924+ellisdesignca@users.noreply.github.com>
2018-03-05 14:43:57 -0500

    Corrected chnages

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@ $(document).foundation()
 
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
+
+
+$('[data-curtain-menu-button]').click(function(){
+  $('body').toggleClass('curtain-menu-open');
+})
+
+
+var wrapperMenu = document.querySelector('.wrapper-menu');
+
+wrapperMenu.addEventListener('click', function(){
+  wrapperMenu.classList.toggle('open');  
+})
+
+wrapperMenu.addEventListener('click', function(){
+  wrapperMenu.classList.toggle('textfast');  
+})
