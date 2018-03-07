d46a430846b34f13ab765842c19fb1a730b43b74
ellisdesignca <34076924+ellisdesignca@users.noreply.github.com>
2018-03-07 15:27:20 -0500

    Started Vendors Page

--- js/app.js
+++ js/app.js
@@ -8,6 +8,23 @@ $('[data-curtain-menu-button]').click(function(){
   $('body').toggleClass('curtain-menu-open');
 })
 
+// shows and hides filtered items
+$(".filter-simple-button").click(function() {
+  var value = $(this).attr('data-filter');
+  if(value === "all") {
+    $('.filter-simple-item').show('1000');
+  } else {
+    $(".filter-simple-item").not('.'+value).hide('3000');
+    $('.filter-simple-item').filter('.'+value).show('3000');
+  }
+});
+
+// changes active class on filter buttons
+$('.filter-simple-button').click(function () {
+  $(this).siblings().removeClass('is-active');
+  $(this).addClass('is-active');
+});
+
 
 var wrapperMenu = document.querySelector('.wrapper-menu');
 
@@ -18,3 +35,7 @@ wrapperMenu.addEventListener('click', function(){
 wrapperMenu.addEventListener('click', function(){
   wrapperMenu.classList.toggle('textfast');  
 })
+
+
+
+
