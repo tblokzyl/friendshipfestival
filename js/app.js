fe111692a3698b6aebb18848a71f74ec122aa10b
ellisdesignca <34076924+ellisdesignca@users.noreply.github.com>
2018-03-14 18:01:58 -0400

    fixed

--- js/app.js
+++ js/app.js
@@ -12,10 +12,10 @@ $('[data-curtain-menu-button]').click(function(){
 $(".filter-simple-button").click(function() {
   var value = $(this).attr('data-filter');
   if(value === "all") {
-    $('.filter-simple-item').show('1000');
+    $('.filter-simple-item').show();
   } else {
-    $(".filter-simple-item").not('.'+value).hide('3000');
-    $('.filter-simple-item').filter('.'+value).show('3000');
+    $(".filter-simple-item").not('.'+value).hide();
+    $('.filter-simple-item').filter('.'+value).show();
   }
 });
 
